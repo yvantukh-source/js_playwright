19df13781e6930a0a2af974dbae21e5b0eddf2c4
Yurii Vantukh <crossbar471@gmail.com>
2024-07-18 20:29:22 +0200

    Added mock for API request

--- tests/my_acc.spec.js
+++ tests/my_acc.spec.js
@@ -5,6 +5,15 @@ import { adminDetaild } from '../data/userDetails.js'
 
 test.only("My acc using coockie injection", async ({page}) => {
     const loginToken = await getLoginToken(adminDetaild.username, adminDetaild.password)
+
+    await page.route("**/api/user/**", async (route, request) => {
+        await route.fulfill({
+            status: 500,
+            contentType: "application/json",
+            body: JSON.stringify({messase: "Playwright error from mocking"})
+        })
+    })
+
     const my_acc = new MyAccountPage(page)
     await my_acc.visit()
     await page.evaluate(([loginTokenInsideBrowserCode]) => {
